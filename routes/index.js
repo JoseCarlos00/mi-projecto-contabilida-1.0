f61d9c9d1e22c8fa4a7ee7361c388ef2456d1dca
carlos <joskar00@hotmail.com>
2023-06-01 01:46:10 -0600

    Update antes de refactorizar con promesas

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', (req, res, next) => {
+  console.log("Paso por aqui");
+  next()
+})
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   console.log(next);
